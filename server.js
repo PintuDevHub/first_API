608b26b75443ff376fe937be83f26a8fca196e8b
PintuDevHub <pintu.dev.vns@gmail.com>
2024-10-27 02:25:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const db = require('./db');
 require('dotenv').config();
-const MenuIteam = require('../nodes/modules/MenuIteam');
+const MenuIteam = require('./modules/MenuIteam');
 const Person = require('../nodes/modules/person');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
